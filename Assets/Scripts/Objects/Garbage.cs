816c08a46920d08fa0414fdca5585a6c7a562441
Pascal Maillard <pascal.maillard@ergon.ch>
2025-01-25 18:13:14 +0100

    add force when colliding with bubbles

--- Assets/Scripts/Objects/Garbage.cs
+++ Assets/Scripts/Objects/Garbage.cs
@@ -18,6 +18,8 @@ namespace Objects
         {
             gameManager = GameManager.Instance;
             GetComponent<Rigidbody2D>().mass = IntialMass;
+
+            Destroy(gameObject, 10);
         }
 
         private void OnCollisionEnter2D(Collision2D collision)
@@ -30,35 +32,43 @@ namespace Objects
             }
 
             //get Name of Collision Object
-            string collisionName = collisionGameObject.tag;
-            switch (tag)
+            switch (collisionGameObject.tag)
             {
                 case "Ship":
                     //Destroy Garbage
-                    Destroy(gameObject);
                     Debug.Log("Garbage has hit the ship");
+                    Destroy(gameObject);
                     gameManager.HandleShipDamage(Damage);
                     break;
                 case BorderManager.Top:
                     //Destroy Garbage
-                    Destroy(gameObject);
                     Debug.Log("Garbage has hit the top");
+                    Destroy(gameObject);
                     break;
                 case "Player":
                 case BorderManager.Bottom:
                     //Lose Life
-                    hasHitBottom = true;
                     Debug.Log("Garbage has hit the bottom");
+                    hasHitBottom = true;
                     gameManager.HandleGarbageDropped();
                     break;
                 case "Bubble":
-                    //Attach Bubble to Garbage
-                    Debug.Log("Bubble has hit the garbage");
-                    collisionGameObject.transform.parent = transform;
-                    collisionGameObject.GetComponent<BubbleProjectile>().enabled = false;
+                    HandleBubbleHit(collisionGameObject);
                     break;
             }
+        }
 
+        private void HandleBubbleHit(GameObject collisionGameObject)
+        {
+            //Attach Bubble to Garbage
+            Debug.Log("Bubble has hit the garbage");
+            collisionGameObject.transform.parent = transform;
+            Destroy(collisionGameObject.GetComponent<BubbleProjectile>());
+            Destroy(collisionGameObject.GetComponent<CircleCollider2D>());
+
+            var force = gameObject.GetComponent<ConstantForce2D>();
+            if (!force) force = gameObject.AddComponent<ConstantForce2D>();
+            force.force += new Vector2(0, 0.5f);
         }
     }
 }
