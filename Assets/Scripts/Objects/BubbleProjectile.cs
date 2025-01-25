7af7a1c11dde53aecbd6946ee9ff7457089e9d26
Janniku9 <jannik@nunu.ai>
2025-01-25 14:37:54 +0100

    fixed background manager prefab & player prefab

--- Assets/Scripts/Objects/BubbleProjectile.cs
+++ Assets/Scripts/Objects/BubbleProjectile.cs
@@ -5,29 +5,54 @@ using UnityEngine;
 public class BubbleProjectile : MonoBehaviour
 {
     [Header("Properties")] [SerializeField, RequiredField]
-    private float moveSpeed = 5f;
+    private float baseSpeed = 5f;
 
+    [SerializeField, RequiredField] private float sideMovementAmplitude = 1f;
+    [SerializeField, RequiredField] private float sideMovementFrequency = 2f;
+    [SerializeField, RequiredField] private float wobbleStrength = 0.2f;
     [SerializeField, RequiredField] private float sizeModifier = 1f;
 
     private float topBounds;
+    private float timeOffset;
+    private Vector3 startPosition;
 
     void Start()
     {
         transform.localScale *= sizeModifier;
-
         var (top, _) = GameBounds.Instance.GetGameBoundsWorldPos();
         topBounds = top;
+
+        // Random starting phase for varied movement
+        timeOffset = Random.Range(0f, 2f * Mathf.PI);
+        startPosition = transform.position;
     }
 
     void Update()
     {
-        transform.position += Vector3.up * (moveSpeed * Time.deltaTime);
+        var time = Time.time + timeOffset;
 
-        // check out of bounds
+        // Basic upward movement
+        var newPosition = transform.position + Vector3.up * (baseSpeed * Time.deltaTime);
+
+        // Sine wave horizontal movement
+        var horizontalOffset = Mathf.Sin(time * sideMovementFrequency) * sideMovementAmplitude;
+
+        // Add some random wobble
+        var randomWobble = Mathf.PerlinNoise(time, timeOffset) * 2f - 1f;
+        horizontalOffset += randomWobble * wobbleStrength;
+
+        // Apply horizontal movement
+        newPosition.x = startPosition.x + horizontalOffset;
+        transform.position = newPosition;
+
         if (transform.position.y > topBounds)
         {
-            // TODO: Handle Reached Top Bounds
-            Destroy(gameObject);
+            HandleTopCollision();
         }
+    }
+
+    private void HandleTopCollision()
+    {
+        Destroy(gameObject);
     }
 }
